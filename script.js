b4d7e71c6d5bf38f2c3cce40b4c4fafbd8ddef2d
ashish070patel <145568083+ashish070patel@users.noreply.github.com>
2025-03-08 20:08:19 +0530

    Add files via upload
    script file updated

---
+++ script.js
@@ -0,0 +1,11 @@
+var typeData = new Typed(".role", {
+    strings: [
+    "Web Developer",
+    "UI-UX Designer",
+    "Coder",
+],
+loop: true,
+typeSpeed: 100,
+backSpeed: 80,
+backDelay: 1000,
+});
